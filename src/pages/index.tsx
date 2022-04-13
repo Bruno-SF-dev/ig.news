1628699880d7bf84585e277c0f1be0991ea7f1fb
Bruno Santos <brunosantosfarias2@gmail.com>
2022-04-12 22:47:11 -0300

    estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
 
-      <h1 className={styles.title}>
+      <h1>
         Hello <span>World</span>
       </h1>
     </>
