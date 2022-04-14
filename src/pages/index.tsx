cdcdf6c8e6c7b8c1766f521e68f4260a6bf1b504
Bruno Santos <brunosantosfarias2@gmail.com>
2022-04-13 21:55:19 -0300

    Static Site Generation na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { stripe } from "../services/stripe";
 
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KoFhYCZWZMH33oObA35FQ61");
 
   const product = {
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24 * 30, // 30 dias
   };
 };
